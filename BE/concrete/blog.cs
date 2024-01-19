5ac4dd2e83335054f8aa9d95f4628250b90109cd
Amir AFSHAR <amir_afshar1@outlook.com>
2024-01-19 18:01:52 +0300

    Add project files.

---
+++ BE/concrete/blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.concrete
+{
+    public class blog
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string TompNailİmage { get; set; }
+        public string İmage {  get; set; }
+        public string CreateDate { get; set; }
+        public bool Status { get; set; }
+
+    }   
+}
