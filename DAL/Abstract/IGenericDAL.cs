ce5bcd9659b17340c2c14da52ba1d77a2c7d883d
Amir AFSHAR <amir_afshar1@outlook.com>
2024-01-22 17:53:21 +0300

    Generik Repostory

---
+++ DAL/Abstract/IGenericDAL.cs
@@ -0,0 +1,19 @@
+﻿using BE.concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Abstract
+{
+    public interface IGenericDAL <T> where T : class
+    {
+        void İnsert(T t);
+        void Delete(T t);   
+        void Update(T t);
+        List<T> GetAll();
+        T GetById(int id);
+
+    }
+}
---
+++ DAL/Repostory/CommentRepository.cs
@@ -0,0 +1,47 @@
+﻿using DAL.Abstract;
+using DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repostory
+{
+    public class CommentRepository<T> : IGenericDAL<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            using var a = new DB();
+            a.Remove(t);
+            a.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            using var a = new DB();
+            return a.Set<T>().ToList();
+
+        }
+
+        public T GetById(int id)
+        {
+            using var a = new DB();
+            return a.Set<T>().Find(id);
+        }
+
+        public void Update(T t)
+        {
+            using var a = new DB();
+            a.Update(t);
+            a.SaveChanges();
+        }
+
+        public void İnsert(T t)
+        {
+            using var a = new DB();
+            a.Add(t);
+            a.SaveChanges();
+        }
+    }
+}
