3d6099511119e9ede95434b6daf737b2c7e525d7
Amir AFSHAR <amir_afshar1@outlook.com>
2024-02-05 18:26:50 +0300

    Update Writer Dashboard

---
+++ asp.netcoreprojce/Controllers/WriterDashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp.netcoreprojce.Controllers
+{
+    public class WriterDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
