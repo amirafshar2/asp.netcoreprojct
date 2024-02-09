4714ac1da3e6814b131882b01d468ae326b34d28
Amir AFSHAR <amir_afshar1@outlook.com>
2024-02-09 17:22:41 +0300

    Update  Notificaton

---
+++ asp.netcoreprojce/Controllers/NotificationController.cs
@@ -0,0 +1,20 @@
+﻿using BLL.Concreate;
+using DAL.EntityFrameWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp.netcoreprojce.Controllers
+{
+    public class NotificationController : Controller
+    {
+        NotificationManager bll = new NotificationManager(new EfNotificationRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult NotificationList()
+        {
+            var Value = bll.GetAll();
+            return View(Value);
+        }
+    }
+}
