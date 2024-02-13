6c163054b8d860e7ec31243cd9a0110b34029abc
Amir AFSHAR <amir_afshar1@outlook.com>
2024-02-13 16:00:54 +0300

    Added Category Create

---
+++ BLL/ValidationRules/CategoryValidaiton.cs
@@ -0,0 +1,19 @@
+﻿using BE.concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ValidationRules
+{
+    public class CategoryValidaiton : AbstractValidator<Category>
+    {
+        public CategoryValidaiton()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Kategory ismi boş bırakılamaz");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Kategory açklaması boş bırakılamaz");
+        }
+    }
+}
--- asp.netcoreprojce/Areas/Admin/Controllers/CategoryController.cs
+++ asp.netcoreprojce/Areas/Admin/Controllers/CategoryController.cs
@@ -1,19 +1,52 @@
-﻿using BLL.Concreate;
+﻿
+
+
+using BE.concrete;
+using BLL.Concreate;
+using BLL.ValidationRules;
 using DAL.EntityFrameWork;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+
 using X.PagedList;
 
 namespace asp.netcoreprojce.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         CategoryManager _bll = new CategoryManager(new EfCategoryRepository());
-        [Area("Admin")]
-        public IActionResult Index(int page = 10)
+       
+        public IActionResult Index(int page = 1)
         {
-            var Value = _bll.GetAll().ToPagedList(page,1);
+            var Value = _bll.GetAll().ToPagedList(page,10);
             return View(Value);
         }
-
+        
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category c)
+        {
+            CategoryValidaiton cv = new CategoryValidaiton();
+            ValidationResult result = cv.Validate(c);
+            if (result.IsValid)
+            {
+                c.Status = true;
+                _bll.İnsert(c);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
     }
 }
